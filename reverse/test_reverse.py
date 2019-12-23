32288cb4ef9cd95cb19bcccef47c5322f6ca65d7
Amir Yunas <ayunas@gmail.com>
2019-12-23 11:02:29 -0600

    ran the test file, I used name prepend instead of add_to_head and append instead of add_to_tail, changed the names of these methods in the tests, and all 5 tests passed successfully.

--- reverse/test_reverse.py
+++ reverse/test_reverse.py
@@ -5,37 +5,37 @@ class LinkedListTests(unittest.TestCase):
   def setUp(self):
     self.list = LinkedList()
 
-  def test_add_to_head(self):
-    self.list.add_to_head(1)
+  def test_prepend(self):
+    self.list.prepend(1)
     self.assertEqual(self.list.head.value, 1)
-    self.list.add_to_head(2)
+    self.list.prepend(2)
     self.assertEqual(self.list.head.value, 2)
 
   def test_contains(self):
-    self.list.add_to_head(1)
-    self.list.add_to_head(2)
-    self.list.add_to_head(10)
+    self.list.prepend(1)
+    self.list.prepend(2)
+    self.list.prepend(10)
     self.assertTrue(self.list.contains(2))
     self.assertTrue(self.list.contains(10))
     self.assertFalse(self.list.contains(1000))
 
   def test_empty_reverse(self):
-    self.list.reverse_list()
+    self.list.reverse()
     self.assertEqual(self.list.head, None)
     
   def test_single_reverse(self):
-    self.list.add_to_head(1)
-    self.list.reverse_list()
+    self.list.prepend(1)
+    self.list.reverse()
     self.assertEqual(self.list.head.value, 1)
 
   def test_longer_reverse(self):
-    self.list.add_to_head(1)
-    self.list.add_to_head(2)
-    self.list.add_to_head(3)
-    self.list.add_to_head(4)
-    self.list.add_to_head(5)
+    self.list.prepend(1)
+    self.list.prepend(2)
+    self.list.prepend(3)
+    self.list.prepend(4)
+    self.list.prepend(5)
     self.assertEqual(self.list.head.value, 5)
-    self.list.reverse_list()
+    self.list.reverse()
     self.assertEqual(self.list.head.value, 1)
     self.assertEqual(self.list.head.get_next().value, 2)
     self.assertEqual(self.list.head.get_next().get_next().value, 3)
